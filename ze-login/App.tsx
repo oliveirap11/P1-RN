ea6f3af1a39237125f53b69e12d499586cf626f1
oliveirap11 <patricks9612@gmail.com>
2025-06-20 22:18:15 -0300

    Finalizando a pagina de login

--- ze-login/App.tsx
+++ ze-login/App.tsx
@@ -1,13 +1,33 @@
 import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native';
 import { styles } from './styles';
 import { LinearGradient } from 'expo-linear-gradient';
+import logo from './assets/logo.png'
+import icon from './assets/icon.png'
+import bebidas from './assets/bebidas.png'
 
 const App = () => {
   return (
-    <LinearGradient colors={['#ffff00', '#ffd700',]} style={styles.container}>
+    <LinearGradient colors={['#ffd700', '#daa520',]} style={styles.fundo}>     
     
     <View >
-      <Text>Olá mundo</Text>
+      <View style={styles.navbar}>
+        <Image source={logo} style={styles.logo} />
+        <Image source={icon} style={styles.icon} />
+      </View>
+      <View>
+        <Image source={bebidas} style={styles.bebidas} />
+      </View>       
+      <View style={styles.container}>
+      <Text style={styles.texto}>
+        Entre ou cadastre-se
+        </Text>
+      <TextInput style={styles.input}>
+        <Text >E-mail</Text>
+      </TextInput>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.ButtonText}>CONTINUAR COM E-MAIL</Text>
+      </TouchableOpacity>
+      </View>
     </View>
     </LinearGradient>
   );
--- ze-login/styles.ts
+++ ze-login/styles.ts
@@ -1,8 +1,67 @@
 import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
+  fundo: {
+    flex:1     
+  },
+  navbar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',    
+    padding: 20,
+    marginTop: 30 
+    
+  },
+  logo: {
+    width: 65,
+    height: 42,
+    marginLeft: 150
+    
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    
+  },
+  bebidas: {
+    width: 350,
+    height: 280,
+    marginTop: 150,
+    marginLeft: 20,
+  },
+  container: {    
     justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 120,
+    padding: 20,
+  },
+  texto: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#000',
+    marginBottom: 10,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderColor: '#fff',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    color: '#9E9E9E',
+    backgroundColor: '#fff',
+    
+  },
+  button: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#D9D9D9',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  ButtonText: {
+    color: '#9E9E9E',
+    fontSize: 15,
+    fontWeight: 'bold',
   },
 });
