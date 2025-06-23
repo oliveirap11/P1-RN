cba9e8b13e0295bf506be0e87f7b5dbea95ef7c7
oliveirap11 <patricks9612@gmail.com>
2025-06-23 03:08:18 -0300

    adicionando os button

--- feed/App.tsx
+++ feed/App.tsx
@@ -2,6 +2,8 @@ import { View, Text, Image, TouchableOpacity, TextInput, ImageBackground } from 
 import { styles } from './styles';
 import fundo from './assets/Fundo.png';
 import icon from './assets/icon.png'
+import logo from './assets/logoTix.png';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 
@@ -12,8 +14,37 @@ const App = () => {
     <View style={styles.perfil}>
       <Image source={icon} style={styles.icon} />
       <Text style={styles.textoIcon}>Olá, Patrick! </Text>
-
-      
+      </View>
+      <View style={styles.logoContainer}>
+      <Image source={logo} style={styles.logo} />
+      </View>
+      <View>
+        <TouchableOpacity style={styles.button}>
+        <Text style={styles.ButtonText}>TIX Teresópolis - RJ</Text>
+      </TouchableOpacity>
+      </View>
+      <View style={styles.containerButton}>
+      <TouchableOpacity style={styles.buttonShopp}>
+        <View style={styles.iconBox}>
+          <Icon name="shopping-outline" size={30} color="#fff" />
+          <View style={styles.separator} />
+        </View>
+        <Text style={styles.buttonText}>DELIVERY</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.buttonShopp}>
+        <View style={styles.iconBox}>
+          <Icon name="store-outline" size={30} color="#fff" />
+          <View style={styles.separator} />
+        </View>
+        <Text style={styles.buttonText}>RETIRADA</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.buttonShopp}>
+        <View style={styles.iconBox}>
+          <Icon name="information-outline" size={30} color="#fff" />
+          <View style={styles.separator} />
+        </View>
+        <Text style={styles.buttonText}>SOBRE NÓS</Text>
+      </TouchableOpacity>
     </View>
     </ImageBackground>
 
--- feed/styles.ts
+++ feed/styles.ts
@@ -5,21 +5,19 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
     height: 80,
-    backgroundColor: '#ff0000',
-    
-    
-  },
+    backgroundColor: '#ff0000',    
+  },  
   fundoImg: {
     width: '100%',
     height: '100%',
-    resizeMode: 'cover',
+    paddingBottom: 45,   
   },
   icon: {
     width: 30,
     height: 30,
     top: 20,
     left: 10,
-    
+            
   },
   textoIcon: {
     position: 'absolute',
@@ -27,6 +25,60 @@ export const styles = StyleSheet.create({
     fontSize: 15,
     top: 50,
     left: 45,
+  },
+  logoContainer: {    
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    top: 120,
+  },
+  logo: {
+    width: 220,
+    height: 220,
+    position: 'absolute',
+  },
+  button: {
+    backgroundColor: 'rgba(255, 45, 85, 0.9)',
+    borderRadius: 40,
+    paddingVertical: 8,
+    paddingHorizontal: 40,
+    marginHorizontal: 40,
+    marginTop: 250,
+    alignItems: 'center',
+  },
+  ButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  containerButton: {    
+    justifyContent: 'center',
+    alignItems: 'center', 
+    gap: 20,
+    marginTop: 20,   
+  },
+  buttonShopp: {
+    flexDirection: 'row',
+    alignItems: 'center',    
+    backgroundColor: 'rgba(255, 45, 85, 0.9)',
+    borderRadius: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    
+  },
+  iconBox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 12,    
+  },
+  separator: {
+    width: 1,
+    height: 30,
+    backgroundColor: '#fff',
+    marginLeft: 10,
+  },
+  buttonText: {
+    color: '#fff',    
+    fontSize: 16,    
   },
 
 });
